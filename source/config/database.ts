60b8acde2ba8a7b2e2401b98fd511f9d7276d422
Fernando Britto <fernandobritto.web@gmail.com>
2021-01-06 08:00:31 -0300

    add: Sequelize ORM

---
+++ source/config/database.ts
@@ -0,0 +1,11 @@
+module.exports = {
+    host: 'localhost',
+    dialect: 'postgres',
+    username: 'postgres',
+    password: '1q2w3e4r',
+    database: 'alphatower',
+    define: {
+        timestamps: true,
+        underscored: true,
+    }
+}
